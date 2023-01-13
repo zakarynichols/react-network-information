29990b5af2987721c5dd9e960509685902b27cf7
Zakary Nichols <zakarynichols@gmail.com>
2023-01-12 23:19:43 -0500

    edit declaration file to match network information api specification; update implementation of hook to match; general improvements

--- networkInfo.d.ts
+++ networkInfo.d.ts
@@ -1,11 +1,4 @@
-declare enum EffectiveConnectionType {
-  "2g",
-  "3g",
-  "4g",
-  "slow-2g"
-}
-
-declare enum ConnectionType {
+enum ConnectionType {
   "bluetooth",
   "cellular",
   "ethernet",
@@ -17,33 +10,28 @@ declare enum ConnectionType {
   "wimax"
 }
 
-/**
- * Some browsers do not support network information or only specific keys.
- * To prevent a runtime error for the consumer; set all properties as optional.
- */
-type NetworkInformation = Partial<{
+enum EffectiveConnectionType {
+  "2g",
+  "3g",
+  "4g",
+  "slow-2g"
+}
+
+interface NetworkInformation extends EventTarget {
   readonly type: ConnectionType
   readonly effectiveType: EffectiveConnectionType
+  readonly downlinkMax: number
   readonly downlink: number
   readonly rtt: number
-  readonly saveData: boolean
-  onchange: ((event: Event & { currentTarget: NetworkInformation }) => void) | null
-  addEventListener(
-    type: "change",
-    callback: (ev: Event & { currentTarget: NetworkInformation }) => void | null,
-    options?: AddEventListenerOptions | boolean
-  ): void
-  dispatchEvent(event: Event): boolean
-  removeEventListener(
-    type: "change",
-    callback: (ev: Event & { currentTarget: NetworkInformation }) => void | null,
-    options?: EventListenerOptions | boolean
-  ): void
-}>
+  onchange: Event
+}
 
-declare interface NavigatorNetworkInformation {
+type Megabit = number
+type Millisecond = number
+
+interface NavigatorNetworkInformation {
   readonly connection: NetworkInformation
 }
 
-declare interface Navigator extends NavigatorNetworkInformation {}
-declare interface Worker extends NavigatorNetworkInformation {}
+interface Navigator extends NavigatorNetworkInformation {}
+interface WorkerNavigator extends NavigatorNetworkInformation {}
--- src/useNetworkInformation.ts
+++ src/useNetworkInformation.ts
@@ -1,53 +1,46 @@
 import { useEffect, useState } from "react"
 
-// Declare in module scope to omit from the deps array.
-const connection = navigator.connection
+type ConnectionChangeEvent = Event & { currentTarget: NetworkInformation }
 
-type OrderedNetworkInfo = {
-  orderId: number
-  info: NetworkInformation
-}
-
-type NetworkEventCallback = ({ currentTarget }: { currentTarget: NetworkInformation }) => void
-
-export function useNetworkInformation(): OrderedNetworkInfo[] {
-  const [networkInfo, setNetworkInfo] = useState<OrderedNetworkInfo[]>([
-    {
-      orderId: 0,
-      info: {
-        effectiveType: connection.effectiveType,
-        downlink: connection.downlink,
-        rtt: connection.rtt,
-        saveData: connection.saveData
-      }
-    }
-  ])
+export function useNetworkInformation(
+  onConnectionChange?: (event: Event) => void
+): NetworkInformation | null {
+  const [networkInfo, setNetworkInfo] = useState<NetworkInformation | null>(null)
 
   useEffect(() => {
-    const onConnectionChange: NetworkEventCallback = ev => {
-      setNetworkInfo(prevNetInfo =>
-        prevNetInfo.concat([
-          {
-            orderId: prevNetInfo.length,
-            info: {
-              effectiveType: ev.currentTarget.effectiveType,
-              downlink: ev.currentTarget.downlink,
-              rtt: ev.currentTarget.rtt,
-              saveData: ev.currentTarget.saveData
-            }
-          }
-        ])
-      )
-    }
-    if (typeof connection.addEventListener === "function")
-      connection.addEventListener("change", onConnectionChange)
-    return () => {
-      if (typeof connection.removeEventListener === "function")
-        connection.removeEventListener("change", onConnectionChange)
-    }
-  }, [])
+    const connection = navigator.connection
 
-  return networkInfo.sort((a, b) => {
-    return a.orderId - b.orderId
-  })
+    if (!("connection" in navigator)) return
+
+    function onConnectionChangeWrapper(event: Event): void {
+      if (isConnectionChangeEvent(event)) {
+        setNetworkInfo(event.currentTarget)
+        onConnectionChange?.(event)
+      }
+    }
+
+    connection.addEventListener("change", onConnectionChangeWrapper)
+    return () => {
+      connection.removeEventListener("change", onConnectionChangeWrapper)
+    }
+  }, [onConnectionChange])
+
+  return networkInfo
+}
+
+function isConnectionChangeEvent(event: Event): event is ConnectionChangeEvent {
+  return "currentTarget" in event && event.currentTarget === navigator.connection
+}
+
+function isNetworkInformation(obj: unknown): obj is NetworkInformation {
+  return (
+    typeof obj === "object" &&
+    obj !== null &&
+    "type" in obj &&
+    "effectiveType" in obj &&
+    "downlinkMax" in obj &&
+    "downlink" in obj &&
+    "rtt" in obj &&
+    "onchange" in obj
+  )
 }
